bd6722cf4e847ed060f03aa579bfd4ca4f68fe51
Weeravaradhana <thaminduweeravardana@gmail.com>
2026-04-09 20:51:31 +0530

    feat(security): configure OAuth2 JWT resource server with stateless sessions

---
+++ src/main/java/com/travel_ease/horel_system/config/SecurityConfig.java
@@ -0,0 +1,40 @@
+package com.travel_ease.horel_system.config;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableWebSecurity
+@EnableMethodSecurity
+@RequiredArgsConstructor
+public class SecurityConfig {
+
+   private final JwtAuthConverter jwtAuthConverter;
+    
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        http
+            .csrf(AbstractHttpConfigurer::disable)
+            .sessionManagement(session ->
+                    session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+            .authorizeHttpRequests(auth -> auth
+                .anyRequest().authenticated()
+            )
+            .oauth2ResourceServer(auth->{
+                auth.jwt(jwtConfigurer ->
+                        jwtConfigurer.jwtAuthenticationConverter(jwtAuthConverter));
+            });
+
+        http.sessionManagement(t->
+                t.sessionCreationPolicy(SessionCreationPolicy.STATELESS));
+        return http.build();
+
+    }
+}
