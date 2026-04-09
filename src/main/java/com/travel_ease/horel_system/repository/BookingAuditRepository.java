bf7d2930fb9dce85d3ab4d46dcd45e7699149f24
Weeravaradhana <thaminduweeravardana@gmail.com>
2026-04-09 20:37:16 +0530

    feat(repository): add BookingAuditRepository for CRUD operations
    - Create BookingAuditRepository extending JpaRepository
    - Supports standard CRUD operations for BookingAudit entity

---
+++ src/main/java/com/travel_ease/horel_system/repository/BookingAuditRepository.java
@@ -0,0 +1,7 @@
+package com.travel_ease.horel_system.repository;
+
+import com.travel_ease.horel_system.entity.BookingAudit;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface BookingAuditRepository extends JpaRepository<BookingAudit,Long> {
+}
