b3067e68606f1501b0d4669089f36c6d0f796193
Weeravaradhana <thaminduweeravardana@gmail.com>
2026-04-09 20:46:04 +0530

    feat(exception): add HotelServiceException for hotel service errors

---
+++ src/main/java/com/travel_ease/horel_system/exception/HotelServiceException.java
@@ -0,0 +1,7 @@
+package com.travel_ease.horel_system.exception;
+
+public class HotelServiceException extends RuntimeException {
+    public HotelServiceException(String message) {
+        super(message);
+    }
+}
